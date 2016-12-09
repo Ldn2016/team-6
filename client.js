888d894317dbf1abee95e269b183815af8ff5571
lewis <lewis-boyd@live.co.uk>
2016-12-09 23:00:12 +0000

    game engine implemented

---
+++ client.js
@@ -0,0 +1,27 @@
+function test() {
+	alert("TEST");
+}
+
+var mainloop = function() {
+  test();
+};
+
+var animFrame = window.requestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.mozRequestAnimationFrame    ||
+  window.oRequestAnimationFrame      ||
+  window.msRequestAnimationFrame     ||
+  null ;
+
+if ( animFrame !== null ) {
+  var recursiveAnim = function() {
+    mainloop();
+    animFrame( recursiveAnim );
+  };
+
+  // start the mainloop
+	animFrame( recursiveAnim );
+} else {
+  var ONE_FRAME_TIME = 1000.0 / 60.0 ;
+  setInterval( mainloop, ONE_FRAME_TIME );
+}
