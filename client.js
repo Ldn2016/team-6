fea98a699b02b90fdf9e4ad60efbee94746c94a8
2140692c <longyin.chow@gmail.com>
2016-12-10 05:34:54 +0000

    fix text width

--- client.js
+++ client.js
@@ -6,6 +6,13 @@ $( document ).ready(function(){
 				$('<li/>', {html: text}).appendTo('#game-chat');
 			}
 			$('#game-form')[0].reset();
+      setTimeout(function(){
+        var li = document.createElement("LI");
+        var response = document.createTextNode("Don't tell me you forgot our meeting today...");
+        li.appendChild(response);
+        document.getElementById("game-chat").appendChild(li);
+      }, 1000);
+      })
 		});
 		$("#game-form").submit(function(e) {
 			e.preventDefault();
